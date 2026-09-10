a8635cbbd323b5189870f350291d3ba8a16833bc
Alvaro Pires de Souza <ap4321316@gmail.com>
2026-09-09 22:22:22 -0300

    chore: adiciona tema inicial para uso da API

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        DB::table('tema')->updateOrInsert(
+            ['id_tema' => 1],
+            [
+                'nome' => 'Matemática Básica',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
     }
 }
